375ce0b49c550dd2c3cc7b1c33b241b5f81446fc
Daniel Santos <daninsky@protonmail.com>
2021-09-01 16:06:50 -0300

    Add Line.

--- src/shapes.h
+++ src/shapes.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include <vector>
+#include <ostream>
 
 #include "point.h"
 
@@ -34,3 +37,20 @@ private:
 	double m_rotation;	// radians
 	obj_type m_type;
 };
+
+class Line {
+public:
+	Line(Point a, Point b) : m_a{a}, m_b{b} { }
+	Point get_a() const { return m_a; }
+	void set_a(Point p) { m_a = p; }
+	Point get_b() const { return m_b; }
+	void set_b(Point p) { m_b = p; }
+private:
+	Point m_a;
+	Point m_b;
+};
+
+std::ostream& operator<<(std::ostream& os, const Line& line)
+{
+	return os << line.get_a() << ", " << line.get_b() << " ----";
+}
