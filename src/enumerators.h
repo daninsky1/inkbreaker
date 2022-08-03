c62f83f73e35389c19abfd636221c63c1604c4f2
Daniel Santos <daninsky12@gmail.com>
2022-08-03 19:20:22 -0300

    Move state enumerators to enumeratos.h

--- src/enumerators.h
+++ src/enumerators.h
@@ -1,6 +1,19 @@
 #pragma once
 
 
+enum class Mode {
+    select,
+    pan,
+    zoom,
+    draw
+};
+
+enum class Select {
+    move,
+    scale,
+    rotate
+};
+
 enum class Draw {
     line,
     rect,
--- src/state.h
+++ src/state.h
@@ -5,13 +5,18 @@
 #define INKBREAKER_VERSION_PATCH 1
 
 #include "objects/shapes.h"
+#include "enumerators.h"
 
 //struct Space2D {
 //    Vector2f offset;
 //};
 
+
 struct InkbreakerState {
     bool changed;
     old::Shape *active_selection;
     std::vector<old::Shape*> latest_selections;
+    Mode mode;
+    Select select;
+    Draw draw;
 };
