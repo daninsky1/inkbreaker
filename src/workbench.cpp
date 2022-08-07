44f79fc472a7984a929bb78f03e5f590c49d41a6
Daniel Santos <daninsky12@gmail.com>
2022-08-07 17:59:17 -0300

    Clean workbench

--- src/workbench.cpp
+++ src/workbench.cpp
@@ -11,27 +11,5 @@ using namespace std;
 using namespace Tree;
 int main()
 {
-    Node *root = new Node();
-    root->name = "I'm the root node!\n";
-    
-    Polygon *poly1 = new Polygon();
-    Node *wrapper1 = new Node(poly1);
-    Polygon *poly2 = new Polygon();
-    Node *wrapper2 = new Node(poly2);
-    Node *node1 = new Node();
-    Node *node2 = new Node();
-    
-    root->add_child(wrapper1);
-    root->add_child(wrapper2);
-    root->add_child(node1);
-    root->add_child(node2);
-    
-    vector<Shape*> shapes;
-    root->get_shapes_dfs(shapes);
-    
-    printf("%lu\n", shapes.size());
-    printf("%d\n", shapes[0] == wrapper1->shape());
-    printf("%d\n", shapes[1] == wrapper2->shape());
-    assert(shapes[0] == wrapper1->shape());
-    assert(shapes[1] == wrapper2->shape());
+    return 0;
 }
