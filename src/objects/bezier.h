8b31380f5ffedde0521ce4bbc07a18431a3ea95b
Daniel Santos <daninsky12@gmail.com>
2022-08-09 16:54:37 -0300

    Rename variables to bhandle

--- src/objects/bezier.h
+++ src/objects/bezier.h
@@ -24,9 +24,9 @@ public:
     void update_bbox() override { };
     bool is_inside_bbox(Vec2f v) override { return false; };
     
-    BezierHandle *add_bhandle(const BezierHandle point);
+    BezierHandle *add_bhandle(const BezierHandle bhandle);
     BezierHandle *add_bhandle(Vec2f point, Vec2f head, Vec2f tail);
-    BezierHandle *insert_bhandle(int index, const BezierHandle point);
+    BezierHandle *insert_bhandle(int index, const BezierHandle bhandle);
     BezierHandle *last_bhandle() { return &bhandles[bhandles.size() - 1]; }
     
     bool empty() { return bhandles.empty(); }
