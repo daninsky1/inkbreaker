c86bb36f4175f866d9aa0b56508e0e5a28ce4900
Daniel Santos <daninsky12@gmail.com>
2022-08-10 19:35:01 -0300

    Fix function return type

--- src/objects/bezier.h
+++ src/objects/bezier.h
@@ -32,7 +32,7 @@ public:
     bool empty() { return bhandles.empty(); }
     void clear() { bhandles.clear(); }
     size_t size() { return bhandles.size(); }
-    void *pop_back() { bhandles.pop_back(); }
+    void pop_back() { bhandles.pop_back(); }
     
     void set_shape_info(ShapeInfo si) { shape_info = si; };
     vector<BezierHandle> bhandles;
