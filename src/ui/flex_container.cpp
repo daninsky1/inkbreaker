18877347a8c706dec63a89536cd1e55b0f047c0c
Daninsky <daninsky12@gmail.com>
2025-07-27 01:50:26 -0300

    feat(ui): Add FlexContainer and Canvas widgets
    This commit introduces two the FlexContainer Widget.
    A powerful layout widget for arranging children in a flexible box model.
    It supports different flex directions (Row, Column, etc.) to distribute
    children along a primary axis. This is a foundational piece for creating
    responsive and complex layouts.
    
    Still WIP.
    
    Key features:
    - FlexContainer inherits from MultiChildWidget to manage multiple children.
    - Implements a render method that arranges children according to the specified
    flexDirection.

---
+++ src/ui/flex_container.cpp
@@ -0,0 +1,41 @@
+#include "flex_container.h"
+
+namespace ui
+{
+void FlexContainer::render(SkCanvas *canvas,uint32_t offsetX,uint32_t offsetY)
+{
+    uint32_t absX = _position.x;
+    uint32_t absY = _position.y;
+    
+    switch (_flexDirection) {
+    case css::FlexDirection::Row:
+        SDL_Log("Rendering FlexContainer in Row direction");
+        for (auto &child : _children) {        
+            if (child) {
+                child->render(canvas,0, 0);
+            }
+            absX += child->getWidth();
+        }
+        break;
+    case css::FlexDirection::Column:
+        SDL_Log("Rendering FlexContainer in Column direction");
+        break;
+    case css::FlexDirection::RowReverse:
+        SDL_Log("Rendering FlexContainer in RowReverse direction");
+        if (_children.empty()) return;
+        
+        for (size_t i = (_children.size() - 1); i > 0; i--) {        
+            auto &child = _children[i];
+            if (child) {
+                child->render(canvas,0, 0);
+            }
+            absX += child->getWidth();
+        }
+        break;
+    case css::FlexDirection::ColumnReverse:
+        SDL_Log("Rendering FlexContainer in ColumnReverse direction");
+        break;
+    }
+}
+
+}
---
+++ src/ui/flex_container.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <include/core/SkPaint.h>
+#include <include/effects/SkDiscretePathEffect.h>
+
+#include "widget.h"
+
+#include "layout.h"
+
+namespace ui
+{
+
+/*
+* Widget FlexContainer.
+* This Widget is intended to hold common some flexbox features.
+*/
+class FlexContainer : public MultiChildWidget
+{
+public:
+    FlexContainer()
+        : _margin(0.0), _padding(0.0) { }
+    ~FlexContainer() = default;
+    
+    void setColor(SkColor4f color) { _backgroundColor = color; }
+    SkColor4f getColor() const { return _backgroundColor; }
+    void setMargin(const css::Margin& margin) { _margin = margin; }
+    const css::Margin& getMargin() const { return _margin;}
+    void setPadding(const css::Padding& padding) { _padding = padding; }
+    const css::Padding& getPadding() const { return _padding; }
+    
+    void render(SkCanvas*canvas,uint32_t offsetX,uint32_t offsetY) override;
+private:
+    SkColor4f _backgroundColor = SkColors::kGray;
+    css::Margin _margin;
+    css::Padding _padding;
+    css::FlexDirection _flexDirection = css::FlexDirection::Row; // Default flex direction
+};
+
+}
